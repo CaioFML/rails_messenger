16f176976187511c554964218614ed0ce934b191
CaioFML <caio.felipemotta@hotmail.com>
2019-12-10 00:33:00 -0300

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import bulmaQuickview from "bulma-quickview/src/js"
+import bulmaQuickview from 'bulma-quickview/src/js'
 
-document.addEventListener("turbolinks:load", function() {
+document.addEventListener('turbolinks:load', function() {
   let quickviews = bulmaQuickview.attach()
 })
