423ba43bd7e6cae50f3baac3ef694ad55b2e6134
CaioFML <caio.felipemotta@hotmail.com>
2019-12-09 23:53:41 -0300

    has_many :contacts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :recoverable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :validatable
+
+  has_many :contacts
 end
