04db11008e04b3059a1d1bec214179f4c649d5e5
CaioFML <caio.felipemotta@hotmail.com>
2019-12-11 17:49:03 -0300

    Adiciona rotas para contacts

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :contacts, only: %i[index new create destroy]
 end
