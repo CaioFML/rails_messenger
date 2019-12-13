e1d9bd5f6ba58c5991195d26d551a83ee5228d16
CaioFML <caio.felipemotta@hotmail.com>
2019-12-13 14:43:16 -0300

    rails g controller chats

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :contacts, only: %i[index new create destroy]
+  resources :chats, only: %i[index show]
 end
