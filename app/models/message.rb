a4e3992c41aa48985d8bef526ff8755422be2bfa
CaioFML <caio.felipemotta@hotmail.com>
2019-12-14 01:54:55 -0300

    Chama job no model message

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,12 @@ class Message < ApplicationRecord
   belongs_to :receiver, class_name: "User"
 
   validates_presence_of :content
+
+  after_create :send_message
+
+  private
+
+  def send_message
+    MessageSenderJob.perform_now(self)
+  end
 end
