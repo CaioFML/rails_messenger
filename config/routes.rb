fbdbf17f04feeea6f940fb45b784ba70e2abfdc5
CaioFML <caio.felipemotta@hotmail.com>
2019-12-14 13:47:18 -0300

    Adiciona configs para utilizar .ngrok

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "chats#index"
-  root to: "chats#index"
 
   get "/manifest.json", to: "service_workers#manifest"
-  get "/service-worker.js", to: "service_workers#service_worker"
+  get "/service-worker.js"  => "service_workers#service_worker"
 
   resources :contacts, only: %i[index new create destroy]
   resources :chats, only: %i[index show] do
