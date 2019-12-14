32231f05485c0d3d9c6d7bdf9a29cdc063ef96a8
CaioFML <caio.felipemotta@hotmail.com>
2019-12-14 12:17:31 -0300

    Adiciona action index em chats controller e view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "chats#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :contacts, only: %i[index new create destroy]
