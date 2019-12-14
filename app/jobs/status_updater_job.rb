0b5321e34a79924930a7b7b8dc71fcffc0e529df
CaioFML <caio.felipemotta@hotmail.com>
2019-12-14 12:56:21 -0300

    Implementa Warden::Manager com o job para alterar o status de user

--- app/jobs/status_updater_job.rb
+++ app/jobs/status_updater_job.rb
@@ -1,7 +1,8 @@
 class StatusUpdaterJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(user)
+    user_status = user.is_online? ? "Online" : "Offline"
+    ActionCable.server.broadcast "user_status_#{user.id}", { status: user_status }
   end
 end
