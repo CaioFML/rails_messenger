2c0a58bc964b1ded2fc166c71bf59cc198391b4c
CaioFML <caio.felipemotta@hotmail.com>
2019-12-14 01:58:25 -0300

    Implementa message_channel.js

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -2,14 +2,12 @@ import consumer from "./consumer"
 
 consumer.subscriptions.create("MessageChannel", {
   connected() {
-    // Called when the subscription is ready for use on the server
-  },
-
-  disconnected() {
-    // Called when the subscription has been terminated by the server
+    console.log("Connected at MessageChannel")
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    messageContainer = document.querySelector("div.message-container")
+    messageContainer.innerHTML += data
+    messageContainer.scrollIntoView(false)
   }
 });
