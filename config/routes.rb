d2cebbfaacce9767bbf086104b473c7fa9675332
CaioFML <caio.felipemotta@hotmail.com>
2019-12-14 13:29:35 -0300

    Adiciona snipets application e routes service_workers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "chats#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "chats#index"
+
+  get "/manifest.json", to: "service_workers#manifest"
+  get "/service-worker.js", to: "service_workers#service_worker"
 
   resources :contacts, only: %i[index new create destroy]
   resources :chats, only: %i[index show] do
