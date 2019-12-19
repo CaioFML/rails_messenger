1df275682fe3a5a942b1768403a0f17a6ce6eb7e
CaioFML <caio.felipemotta@hotmail.com>
2019-12-18 21:37:49 -0300

    Correção

--- db/migrate/20191219003439_add_read_to_messsages.rb
+++ db/migrate/20191219003439_add_read_to_messsages.rb
@@ -1,5 +1,5 @@
 class AddReadToMesssages < ActiveRecord::Migration[6.0]
   def change
-    add_column :messsages, :read, :boolean, default: false
+    add_column :messages, :read, :boolean, default: false
   end
 end
