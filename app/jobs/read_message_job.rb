69d4d5efd644e59fcea49775d9f3d153f2e8ca8f
CaioFML <caio.felipemotta@hotmail.com>
2019-12-18 22:23:28 -0300

    corrige job

--- app/jobs/read_message_job.rb
+++ app/jobs/read_message_job.rb
@@ -2,6 +2,6 @@ class ReadMessageJob < ApplicationJob
   queue_as :default
 
   def perform(user, message_ids = [])
-    ActionCable.server.broadcast "my_messages>#{user.id}", messages: message_ids
+    ActionCable.server.broadcast "my_messages:#{user.id}", messages: message_ids
   end
 end
